6ead249647e458ebc53b789b32df6dfdaaa9e2f2
Ryan RC <murphyslaw480@gmail.com>
2014-03-14 10:36:28 -0400

    more hw3 solns

--- assignment3_solns/part1.sql
+++ assignment3_solns/part1.sql
@@ -14,5 +14,50 @@ GROUP BY athlete;                                 -- per athlete
 SELECT DISTINCT athlete   -- don't repeat an athlete name
 FROM Olympics
 WHERE total_medals > 0      -- won at least 1 medal
-  AND YEAR > 2010           -- 
+  AND YEAR > 2010
   AND athlete IS NOT NULL;  -- filter out NULL athletes
+
+-- Query 4: 26.5ms
+SELECT athlete, sum(gold_medals) AS total_gold
+FROM Olympics
+GROUP BY athlete
+ORDER BY total_gold desc;
+
+-- Query 5: 14.7ms
+SELECT sport
+FROM Olympics
+GROUP BY sport
+ORDER BY sum(total_medals) desc
+LIMIT 1;
+
+-- or (not sure if correct)
+SELECT sport FROM SprotsSumView s
+WHERE SUM >= ALL(SELECT medals from SportsSumView)
+CREATE NEW SportsSumView AS
+SELECT sport, sum(total_medals) as medals
+FROM O
+GROUP BY sport;
+
+-- Query 6: 5.52ms
+SELECT country
+FROM Olympics
+WHERE year >= 2000
+GROUP BY country
+ORDER BY sum(gold_medals) desc
+LIMIT 3;
+
+-- could use RANK : aggregate function that operates on a window
+SELECT country, SUM(gold_medals),
+RANK() OVER(ORDER BY SUM(gold_medals) DESC) AS rank
+FROM Olympics
+WHERE year >= 2000
+GROUP BY Country
+HAVING rank <= 3;
+
+-- Partition Example (Not part of HW)
+SELECT country, SUM(gold_medals),
+RANK() OVER(ORDER BY SUM(gold_medals) DESC) AS rank
+FROM Olympics
+WHERE year >= 2000
+GROUP BY Country, year
+HAVING rank <= 3;
